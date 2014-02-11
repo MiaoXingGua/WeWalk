36f378bda641d33ebc55fb77e1fbe07d4fdec5eb
zerozaki0752 <286995294@qq.com>
2014-02-11 12:59:23 +0800

    增加OSX上传图片软件（未完成）

---
+++ OSX/Bazaar_uploadImage/Bazaar_uploadImage/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Bazaar_uploadImage
+//
+//  Created by Albert on 14-2-10.
+//  Copyright (c) 2014年 Albert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
