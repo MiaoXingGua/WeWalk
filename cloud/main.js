7d38d08203988089b6b63c75d1f5a1680466a232
zerozaki0752 <286995294@qq.com>
2014-02-17 14:46:34 +0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -371,7 +371,7 @@ AV.Cloud.beforeSave("Schedule", function(request, response) {
         response.success();
     }
 
-    createPush([userId],pushDate,"你有一条新得提醒",function(push,error){
+    createPush([userId],pushDate,schedlue.get('content').get('text'),function(push,error){
 
         if (push && !error)
         {
