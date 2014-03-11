dd27a7069a789b6d6f196450f18fafb2624caaa6
zerozaki0752 <286995294@qq.com>
2014-03-11 14:14:37 +0800

    0.0.1.4
    修改查询备忘录，返回的是所有人备忘录问题

--- cloud/main.js
+++ cloud/main.js
@@ -392,6 +392,7 @@ AV.Cloud.define("tickler_date", function(request, response){
     ticklerQuery.greaterThanOrEqualTo('remindTime',date1);
     ticklerQuery.lessThanOrEqualTo('remindTime',date2);
     ticklerQuery.limit(1000);
+    ticklerQuery.equalTo('user',user);
     ticklerQuery.ascending('createdTime');
 
     ticklerList = new Array();
